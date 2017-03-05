1db2ae1330145f9dc3f4a2db12cf6e2f94945e0c
Patrick Santos <jvpspt.ps@gmail.com>
2017-03-05 14:16:15 -0300

    add post for users & get user per id

--- app.js
+++ app.js
@@ -20,6 +20,31 @@ server.get('/', function(req, res, next) {
     return next()
 })
 
+server.get('/user/:id', function(req, res, next) {
+    
+    var response = {
+        'status': 'success',
+        'data': users[parseInt(req.params.id)]
+    }
+
+    res.setHeader('content-type', 'application/json')
+    res.writeHead(200)
+    res.end(JSON.stringify(response))
+    return next()
+})
+
+server.post('/user', function(req, res, next) {
+    var user = req.params
+    max_user_id++
+    user.id = max_user_id
+    users[user.id] = user
+
+    res.setHeader('content-type', 'application/json')
+    res.writeHead(200)
+    res.end(JSON.stringify(user))
+    return next()
+})
+
 server.listen(8080, function() {
     console.log('listening at', server.name, server.url)
 })

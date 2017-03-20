f9fa1afca8392404f4811c9b3f35d123601d9313
Patrick Santos <jvpspt.ps@gmail.com>
2017-03-19 22:38:45 -0300

    add restify-validator import inside app

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var restify = require('restify')
 var server = restify.createServer()
 var setup = require('./controllers/setup.js')
 var userController = require('./controllers/userController.js')
+var restifyValidator = require('restify-validator')
 
-setup(server, restify)
+setup(server, restify, restifyValidator)
 userController(server)
 
 server.listen(8080, function () {

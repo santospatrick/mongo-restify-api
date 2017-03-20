fb240248d07bac8575aea4ecf23fbfd515c2fc8c
Patrick Santos <jvpspt.ps@gmail.com>
2017-03-19 22:39:44 -0300

    add restify-validator into server

--- controllers/setup.js
+++ controllers/setup.js
@@ -1,5 +1,6 @@
-module.exports = function (server, restify) {
+module.exports = function (server, restify, restifyValidator) {
   server.use(restify.acceptParser(server.acceptable))
   server.use(restify.bodyParser())
   server.use(restify.queryParser())
+  server.use(restifyValidator)
 }

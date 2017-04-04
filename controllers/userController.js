b54046339ebe31d1499e79dfe25f322f5c95ee35
Patrick Santos <jvpspt.ps@gmail.com>
2017-04-03 23:40:05 -0300

    update user delete connecting to mongoose

--- controllers/userController.js
+++ controllers/userController.js
@@ -72,15 +72,22 @@ module.exports = function (server) {
   })
 
   server.del('user/:id', function (req, res, next) {
-    req.assert('id', 'Id is required and must be numeric').notEmpty().isInt()
+    req.assert('id', 'Id is required and must be numeric').notEmpty()
     var errors = req.validationErrors()
     if (errors) {
       helpers.failure(res, next, errors[0], 400)
     }
-    if (typeof (users[req.params.id]) === 'undefined') {
-      helpers.failure(res, next, 'the specified user could not be found', 404)
-    }
-    delete users[parseInt(req.params.id)]
-    helpers.success(res, next, [])
+    UserModel.findOne({ _id: req.params.id }, function (err, user) {
+      if (err) {
+        helpers.failure(res, next, 'Something went wrong while fetching the user from the database', 500)
+      }
+      if (user === null){
+        helpers.failure(res, next, 'The specified user could not be found', 404)
+      }
+      user.remove(function(err){
+        helpers.failure(res, next, 'Error removing user from the database', 500)
+      })
+      helpers.success(res, next, user)
+    })
   })
 }
